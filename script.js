e5689a19a88f19eb24e2a2099dde0ed1d0be48f9
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2023-07-25 09:11:07 +0100

    added icons

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+// Variable to store the current drawing data
+let drawingData = [];
+
+// Load saved drawing data from Local Storage, if available
+loadDrawingData();
+
 // Add event listeners for mouse interactions (you'll implement these in your code)
 canvas.addEventListener('mousedown', handleMouseDown);
 canvas.addEventListener('mousemove', handleMouseMove);
@@ -20,3 +26,48 @@ function handleMouseUp(event) { /* ... */ }
 function handleTouchStart(event) { /* ... */ }
 function handleTouchMove(event) { /* ... */ }
 function handleTouchEnd(event) { /* ... */ }
+
+// Function to save the drawing data to Local Storage
+function saveDrawingData() {
+  localStorage.setItem('drawingData', JSON.stringify(drawingData));
+}
+
+// Function to load drawing data from Local Storage
+function loadDrawingData() {
+  const savedDrawingData = localStorage.getItem('drawingData');
+  if (savedDrawingData) {
+    drawingData = JSON.parse(savedDrawingData);
+    redrawCanvas(); // Redraw the canvas with the loaded drawing data
+  }
+}
+
+// Function to redraw the canvas with the loaded drawing data
+function redrawCanvas() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Redraw each stroke from the drawing data
+  drawingData.forEach(stroke => {
+    ctx.strokeStyle = stroke.color;
+    ctx.lineWidth = stroke.lineWidth;
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+    ctx.beginPath();
+    for (let i = 0; i < stroke.points.length; i++) {
+      const point = stroke.points[i];
+      if (i === 0) {
+        ctx.moveTo(point.x, point.y);
+      } else {
+        ctx.lineTo(point.x, point.y);
+      }
+    }
+    ctx.stroke();
+  });
+}
+
+// Function to clear the canvas and reset drawing data
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawingData = [];
+  saveDrawingData();
+}
